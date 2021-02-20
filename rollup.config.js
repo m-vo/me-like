5bb9a23d8742d48316079e172e6dbf1de3716a9c
M. Vondano <m-vo@users.noreply.github.com>
2021-02-20 23:37:34 +0100

    add minimal css resource

--- rollup.config.js
+++ rollup.config.js
@@ -5,19 +5,30 @@ import scss from 'rollup-plugin-scss'
 import typescript from '@rollup/plugin-typescript';
 import {terser} from "rollup-plugin-terser";
 
-export default {
-    input: 'layout/main.ts',
-    output: {
-        file: 'src/Resources/public/like_widget.js',
-        format: 'iife',
-        intro: 'const global = window;'
+export default [
+    {
+        input: 'layout/main.ts',
+        output: {
+            file: 'src/Resources/public/like_widget.js',
+            format: 'iife',
+            intro: 'const global = window;'
+        },
+        plugins: [
+            builtins(),
+            resolve(),
+            commonjs(),
+            typescript(),
+            terser(),
+            scss(),
+        ],
     },
-    plugins: [
-        builtins(),
-        resolve(),
-        commonjs(),
-        typescript(),
-        terser(),
-        scss(),
-    ],
-}
+    {
+        input: 'layout/styles/like_widget_minimal.scss',
+        output: {
+            file: 'src/Resources/public/like_widget_minimal',
+        },
+        plugins: [
+            scss(),
+        ],
+    }
+]
